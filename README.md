4f6681f4ee8a9bb46128ba77d8358aa88aaefc32
cstml <cstmlcodes@gmail.com>
2021-07-29 18:16:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# A Rust Linked List 
+
+This library contains a simple Rust implementation of: 
+
+- a linked list;
+- a polymorphic linked list;
+
+Furthermore, if you run the file, you will see that the executable runs a small
+example that shows how the functions work. 
+
+## How to build 
+
+You can simply build it like you would any other project with: 
+
+```bash
+cargo run
+# or 
+cargo build && ./target/debug/linked_list_example
+```
+
+## But why?
+
+The first thing I wanted to create when I got into Rust was a linked list. I
+remember at the time being unable to do so, and then asking loads of people -
+with the answer not actually being given. But the time has come, and I was able
+to bash this out, so I thought - why not - let's put it on github.
+
+
+## Anything else?
+
+No, that's it. 
+
+Bye.
+
---
+++ app/Main.rs
@@ -0,0 +1,33 @@
+#![allow(non_snake_case)]
+use linked_list as lib;
+use lib::i32_linked_list as i;
+use lib::polymorphic_linked_list as p;
+
+pub fn main () {
+    
+    let _fst = i::create_1node(1); 
+    let _snd = i::create_1node(2);
+    let _trd = i::create_1node(3);
+    let _frt = i::create_node();
+
+    let mut fst = i::link_nodes (_fst, _snd);
+    fst = i::link_nodes (fst, _trd);
+    fst = i::link_nodes (fst, i::create_node());
+    fst = i::link_nodes (fst, _frt);
+
+    println!("{:?}", fst);
+
+    let _pFst = p::create_1node::<bool> (true);
+    let _pSnd = p::create_1node::<bool> (false);
+    let pFst = p::link_nodes (_pFst, _pSnd);
+
+    println!("{:?}", pFst);
+
+    // Polymorphic Node of Nodes 
+    let _pnFst = p::create_1node::<i::Node> (i::create_node());
+    let _pnSnd = p::create_1node::<i::Node> (i::create_1node(1));
+    // Linking Polymorphic Node of Nodes 
+    let pnFst = p::link_nodes (_pnFst, _pnSnd);
+
+    println!("And the last one: {:?}", pnFst);
+}
